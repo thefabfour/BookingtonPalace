9e7a0e69d2c7c16558cb910559d9ef9e462a4309
Anthony Patterson <pattersonanthony14@gmail.com>
2021-01-15 21:14:52 -0500

    Updated to have all data/ server functionality for mockData

---
+++ database/Users.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+// const db = require('./index.js');
+
+const usersSchema = new mongoose.Schema({
+  user_id: Number,
+  firstName: String,
+  lastName: String,
+  email: String,
+  profileUrl: String,
+  pictureUrl: String,
+  password: String,
+});
+
+const Users = mongoose.model('Users', usersSchema);
+
+module.exports = Users;
