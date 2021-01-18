2c634440391ddd925675e8a804ad892bedf6124c
Anthony Patterson <pattersonanthony14@gmail.com>
2021-01-18 17:56:28 -0500

    Refactored to have Hooks. Working MVP

--- client/src/components/categoryButtons/CategoryControl.jsx
+++ client/src/components/categoryButtons/CategoryControl.jsx
@@ -1,38 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import ButtonsDisplay from './ButtonsDisplay.jsx'
 
-export default function categoryControl(props) {
-  const [isLongList, setIsLongList] = useState(false);
-  useEffect(() => {
-    if (props.categories.length >= 5) {
-      setIsLongList(true);
-    }
-  }, []);
+  export default function categoryControl(props) {
+    const [isLongList, setIsLongList] = useState(false);
+    useEffect(() => {
+      if (props.categories.length >= 5) {
+        setIsLongList(true);
+      }
+    }, []);
 
-  const [isShowMoreClicked, setIsShowMoreClicked] = useState(false);
+    const [isShowMoreClicked, setIsShowMoreClicked] = useState(false);
 
-  const handleShowMoreClick = () => {
-    setIsShowMoreClicked(!isShowMoreClicked);
-  };
+    const handleShowMoreClick = () => {
+      setIsShowMoreClicked(!isShowMoreClicked);
+    };
 
-  if (isLongList) {
-    let lengthList;
+    if (isLongList) {
+      let lengthList;
 
-    if (isShowMoreClicked) {
-      lengthList = props.categories;
-    } else {
-      lengthList = props.categories.slice(0, 4);
+      if (isShowMoreClicked) {
+        lengthList = props.categories;
+      } else {
+        lengthList = props.categories.slice(0, 4);
+      }
+      return (
+        <div>
+          <ButtonsDisplay categories={lengthList} />
+          <button type="button" onClick={handleShowMoreClick}> {isShowMoreClicked ? 'Show Less' : 'Show More'} </button>
+        </div>
+      );
     }
     return (
       <div>
-        <ButtonsDisplay categories={lengthList} />
-        <button type="button" onClick={handleShowMoreClick}> {isShowMoreClicked ? 'Show Less' : 'Show More'} </button>
+        <ButtonsDisplay categories={props.categories} />
       </div>
     );
   }
-  return (
-    <div>
-      <ButtonsDisplay categories={props.categories} />
-    </div>
-  );
-}
