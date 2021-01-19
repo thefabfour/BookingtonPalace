b3a4e073328df2aaa7e1e0c3411c1b29e926303a
Anthony Patterson <pattersonanthony14@gmail.com>
2021-01-18 21:24:10 -0500

    Deconstructed two lines on CategoryControl.jsx

--- client/src/components/categoryButtons/CategoryControl.jsx
+++ client/src/components/categoryButtons/CategoryControl.jsx
@@ -1,38 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import ButtonsDisplay from './ButtonsDisplay.jsx'
 
-  export default function categoryControl(props) {
-    const [isLongList, setIsLongList] = useState(false);
-    useEffect(() => {
-      if (props.categories.length >= 5) {
-        setIsLongList(true);
-      }
-    }, []);
+export default function categoryControl({ categories }) {
+  const [isLongList, setIsLongList] = useState(false);
+  useEffect(() => {
+    if (categories.length >= 5) {
+      setIsLongList(true);
+    }
+  }, []);
 
-    const [isShowMoreClicked, setIsShowMoreClicked] = useState(false);
+  const [isShowMoreClicked, setIsShowMoreClicked] = useState(false);
 
-    const handleShowMoreClick = () => {
-      setIsShowMoreClicked(!isShowMoreClicked);
-    };
+  const handleShowMoreClick = () => {
+    setIsShowMoreClicked(!isShowMoreClicked);
+  };
 
-    if (isLongList) {
-      let lengthList;
+  if (isLongList) {
+    let lengthList;
 
-      if (isShowMoreClicked) {
-        lengthList = props.categories;
-      } else {
-        lengthList = props.categories.slice(0, 4);
-      }
-      return (
-        <div>
-          <ButtonsDisplay categories={lengthList} />
-          <button type="button" onClick={handleShowMoreClick}> {isShowMoreClicked ? 'Show Less' : 'Show More'} </button>
-        </div>
-      );
+    if (isShowMoreClicked) {
+      lengthList = categories;
+    } else {
+      lengthList = categories.slice(0, 4);
     }
     return (
       <div>
-        <ButtonsDisplay categories={props.categories} />
+        <ButtonsDisplay categories={lengthList} />
+        <button type="button" onClick={handleShowMoreClick}> {isShowMoreClicked ? 'Show Less' : 'Show More'} </button>
       </div>
     );
   }
+  return (
+    <div>
+      <ButtonsDisplay categories={categories} />
+    </div>
+  );
+}
