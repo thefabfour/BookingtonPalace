dc87c837b67c2eaeddd13fe80e0b63f597948399
Anthony Patterson <pattersonanthony14@gmail.com>
2021-01-19 00:05:50 -0500

    Updated server, database modules, and seed file to only have one collection with the users embedded with review data. Spent a long time configuring the category generation. For some reason not rendering in mongoDB, despite rendering in node.js correctly. Attacking tomorrow

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,57 +8,25 @@ class App extends React.Component {
     this.state = {
       reviews: [],
       users: [],
-      categories: [
-        {title: "Responsive host",
-        count: 2},
-        {title: "Great location",
-        count:5},
-        {title: " Helpful host",
-        count: 12},
-        {title: "Comfortable beds",
-        count: 12},
-        {title: "Easy check-in",
-        count: 15},
-        {title: "Great views",
-        count: 17},
-        {title: "A quiet neighborhood",
-        count: 1},
-        {title: "Central location",
-        count: 19},
-        {title: "Thoughtful touches",
-        count: 12}
-      ]
+      categories: [],
     };
-
-     // bind statements go here
-
   }
 
   componentDidMount() {
     axios.get('/api/listing/reviews')
       .then((response) => {
-        // console.log(response);
-        this.setState({reviews: response.data})
-        // console.log('the state is now', this.state)
+        console.log(response);
+
+        this.setState({
+          reviews: response.data,
+        })
+        console.log('the state is now', this.state)
       })
-      .catch(function(error) {
+      .catch((error) => {
         if (!error.status) {
-          // network error
+          return;
         }
       });
-
-    axios.get('/api/users')
-      .then((users) => {
-        // console.log(users)
-        this.setState({users: users.data})
-        // console.log('the state is now', this.state)
-      })
-      .catch(function(error) {
-        if (!error.status) {
-          // network error
-        }
-      });
-
   }
 
   render() {
@@ -73,4 +41,5 @@ class App extends React.Component {
 }
 
 export default App;
+
 
