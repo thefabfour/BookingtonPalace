99c18053c560cb55e6f1407f3510ba9668316e67
Anthony Patterson <pattersonanthony14@gmail.com>
2021-01-20 13:12:41 -0500

    Commented out ReviewRender.jsx. This is in preparation for a userReviews refactor

--- client/src/components/userReviews/reviewRender/ReviewRender.jsx
+++ client/src/components/userReviews/reviewRender/ReviewRender.jsx
@@ -1,76 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import classes from '../UserReviews.module.css';
-import moment from 'moment';
-
-export default function ReviewRender({profilePicture, firstName, entryDate}){
-
-  const handleShowMoreClick = () => {
-  setIsShowMoreClicked(!isShowMoreClicked);
-};
-
-  let lengthReview;
-
-  if (isShowMoreClicked) {
-    lengthReview = review.body;
-  } else {
-    lengthReview = review.body.slice(0,180) + "..."
-  }
+// preparation for refactoring
 
 
-  function Header=() {
-    return(
-      <div>
-       <img className={classes.userPhoto} src={profilePicture}/>
-       <div>{firstName}</div>
-       <div>
-         {moment(entryDate).format("MMMM YYYY")}
-      </div>
-    </div>
-    )
-  }
 
-  function LongReview=({}) {
-      return (
-      <div>
-            <ReviewHeader review={review}/>
-            <div>{lengthReview}</div>
-        <button type="button" onClick={handleShowMoreClick}> {isShowMoreClicked ? 'read less' : 'read more'} </button>
-      </div>
-      )
-  }
+// import React, { useState, useEffect } from 'react';
+// import classes from '../UserReviews.module.css';
+// import moment from 'moment';
 
-  function ShortReviewRender ({}){
-    return (
-      <div>
-        <ReviewHeader review={props.review}/>
-        <div>{props.review.body}</div>
-      </div>
-    )
-  }
-
-// export default ReviewsHeader;
-
-
-// export default function LongReviewRender({review}) {
-//   const [isShowMoreClicked, setIsShowMoreClicked] = useState(false);
+// export default function ReviewRender({profilePicture, firstName, entryDate}){
 
 //   const handleShowMoreClick = () => {
-//     setIsShowMoreClicked(!isShowMoreClicked);
-//   };
+//   setIsShowMoreClicked(!isShowMoreClicked);
+// };
 
-//     let lengthReview;
+//   let lengthReview;
 
-//     if (isShowMoreClicked) {
-//       lengthReview = review.body;
-//     } else {
-//       lengthReview = review.body.slice(0,180) + "..."
-//     }
+//   if (isShowMoreClicked) {
+//     lengthReview = review.body;
+//   } else {
+//     lengthReview = review.body.slice(0,180) + "..."
+//   }
 
-//     return (
+
+//   function Header=() {
+//     return(
+//       <div>
+//        <img className={classes.userPhoto} src={profilePicture}/>
+//        <div>{firstName}</div>
+//        <div>
+//          {moment(entryDate).format("MMMM YYYY")}
+//       </div>
+//     </div>
+//     )
+//   }
+
+//   function LongReview=({}) {
+//       return (
 //       <div>
 //             <ReviewHeader review={review}/>
 //             <div>{lengthReview}</div>
 //         <button type="button" onClick={handleShowMoreClick}> {isShowMoreClicked ? 'read less' : 'read more'} </button>
 //       </div>
-//     );
+//       )
+//   }
+
+//   function ShortReviewRender ({}){
+//     return (
+//       <div>
+//         <ReviewHeader review={props.review}/>
+//         <div>{props.review.body}</div>
+//       </div>
+//     )
 //   }
