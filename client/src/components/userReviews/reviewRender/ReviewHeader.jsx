7500f115ab6c60940ac394ac31f9cc098f2b5ff5
Anthony Patterson <pattersonanthony14@gmail.com>
2021-01-21 14:03:16 -0500

    Ready for merge. Need to refine small points on arrow in userReviews. This will come during refactor. Need to fix small points regarding incoming data to make only 6 category bars....removing number reviews

--- client/src/components/userReviews/reviewRender/ReviewHeader.jsx
+++ client/src/components/userReviews/reviewRender/ReviewHeader.jsx
@@ -5,8 +5,13 @@ import moment from 'moment';
 const ReviewsHeader = (props) => (
      <div className={classes.header}>
        <img className={classes.userPhoto} src={props.review.user_info.pictureUrl + "?dummy=8484744"} />
-       <div>{props.review.user_info.firstName}</div>
-       <div> {moment(props.review.entry_date).format("MMMM YYYY")}</div>
+       <div className={classes.textContainer}>
+       {props.review.user_info.firstName}
+
+          <div className={classes.date}>
+            {moment(props.review.entry_date).format("MMMM YYYY")}
+          </div>
+       </div>
     </div>
 )
 
