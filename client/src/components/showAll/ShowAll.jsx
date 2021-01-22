f22101c2d89cc1b20d6df78dd6d54729beffdf33
Anthony Patterson <pattersonanthony14@gmail.com>
2021-01-22 00:58:07 -0500

    Separated ShowAllHeader into separate file. Cleaned up rendering in ShowAll.

--- client/src/components/showAll/ShowAll.jsx
+++ client/src/components/showAll/ShowAll.jsx
@@ -1,56 +1,33 @@
 import React from 'react';
-import axios from 'axios';
 
 import CategoryControl from '../categoryButtons/CategoryControl';
 import CategoryGraphs from '../categoryGraphs/CategoryGraphs';
 import UserReviews from '../userReviews/UserReviews';
-import Backdrop from './backdrop/Backdrop'
-import classes from './ShowAll.module.css'
-import {BsX} from 'react-icons/bs'
-
+import Backdrop from './backdrop/Backdrop';
+import ShowAllHeader from './ShowAllHeader';
+import classes from './ShowAll.module.css';
+import {BsX} from 'react-icons/bs';
 
 export default function ShowAll({show, close, children}) {
-  console.log(children)
   return (
-
     <>
-      <Backdrop show={show} clicked={close}/>
+      <Backdrop show={show} clicked={close} />
       <div className={classes.modal}
-      style={{transform: show ? 'translateY(0)' : 'translateY(-100vh)',opacity: show ? '1' : '0',}}>
-
-                    <div className={classes.exitBar}>
-                      <BsX type="button" onClick={close} className={classes.exitBtn}/>
-
-
-                              <div className={classes.modalHeader}>
-                                        <div className={classes.starAndText}>
-
-                                            <span className={classes.star}> &#9733;</span>
-
-                                            <span className={classes.header}> 4.83 (70 reviews) </span>
-                                        </div>
-                                        <div className={classes.searchBarContainer}>
-                                          <form className={classes.searchBar}>
-                                            <input  className={classes.searchBar} placeholder="Search reviews">
-                                            </input>
-
-                                          </form>
-                                        </div>
-                              </div>
-
-
-                              <div className={classes.something}>
-                                        <div className={classes.graphsAndButtonContainer}>
-                                          {children[0]}
-                                          {children[1]}
-                                        </div>
-                                        <div className={classes.reviewsContainer}>
-                                          {children[2]}
-                                        </div>
-                              </div>
-                    </div>
+        style={{transform: show ? 'translateY(0)' : 'translateY(-100vh)', opacity: show ? '1' : '0',}}>
+        <div className={classes.exitBar}>
+          <BsX type="button" onClick={close} className={classes.exitBtn} />
+          <ShowAllHeader />
+          <div className={classes.something}>
+            <div className={classes.graphsAndButtonContainer}>
+              {children[0]}
+              {children[1]}
+            </div>
+            <div className={classes.reviewsContainer}>
+              {children[2]}
+            </div>
+          </div>
         </div>
-
+      </div>
     </>
   );
 }
