a169681ff44a4ffe6b1da34d781083b60ed8b582
Anthony Patterson <pattersonanthony14@gmail.com>
2021-01-21 23:39:10 -0500

    Merged with master

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
 import App from '../client/src/components/App'
+
 describe('App', () => {
   it('renders the App component', () => {
     render(<App />);
