9e879f3a0e0e2b5b29a30b499ee63f4528f0b640
Anthony Patterson <pattersonanthony14@gmail.com>
2021-01-27 18:23:24 -0500

    Style css update

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
             loader: 'css-loader',
             options: {
               modules: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]',
             },
           },
         ],
