ab688deda6f69424fb44481b7faf32f6d78676a0
Anthony Patterson <pattersonanthony14@gmail.com>
2021-01-27 18:46:37 -0500

    Further changes to webpack, css style loader config

--- webpack.config.js
+++ webpack.config.js
@@ -20,12 +20,14 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]',
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:5]',
+              },
             },
           },
         ],
-      },
+      }
     ],
   },
   resolve: {
